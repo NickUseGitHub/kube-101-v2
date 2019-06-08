ce6951f972aa0218524ba6bc0b828f4bfc80f1c0
nicknoi Tencent <chaiwat@tencent.co.th>
2019-06-09 00:06:39 +0700

    [init] simple app and rest

--- app/index.js
+++ app/index.js
@@ -1,14 +1,21 @@
+const koaRoute = require('koa-route');
 const Koa = require('koa');
 const app = new Koa();
 
 const serverName = process.env.SERVER_NAME;
 const port = process.env.PORT; 
 
-// response
-app.use(ctx => {
+app.use(koaRoute.get('/appdata', function(ctx) {
+  ctx.set('Content-Type', 'application/json');
+  ctx.body = JSON.stringify({
+    name: serverName
+  });
+}));
+
+app.use(koaRoute.get('/', function(ctx) {
   ctx.set('Content-Type', 'text/html; charset=utf-8');
   ctx.body = `Hello Koa V.1 ${serverName}. <br /> Your path is ${ctx.request.url}`;
-});
+}));
  
 app.listen(port);
 
